31b597ef974a8d9139f281a767d22e7b7c45d3a8
Valdomiro Medeiros Costa <valdomiromc@yahoo.com.br>
2022-02-20 11:51:24 -0300

    Salvando e Lendo Objetos pela rede
    Salvando e lendo objetos pela rede e novidades do Java 7 como Diamond, Separador de Digitos, Switch Case entre outros

---
+++ Estudo_Java/src/com/valdomiro/curso/novidadesdojava/ExemploSeparadorDigitosLiterais.java
@@ -0,0 +1,21 @@
+package com.valdomiro.curso.novidadesdojava;
+
+public class ExemploSeparadorDigitosLiterais {
+
+	public static void main(String[] args) {
+		long populacaoSaoPaulo = 11_000_000L;
+		long populacaoGrandeSaoPaulo = 11_000_000 + 9_000_000;
+		
+		System.out.printf("População São Paulo: %d\n", populacaoSaoPaulo);
+		System.out.printf("População da Grande São Paulo: %d\n", populacaoGrandeSaoPaulo);
+		
+		double precoVeiculo = 200_000.99_1d;
+		System.out.printf("Preço veículo: %.3f\n", precoVeiculo);
+		
+		// Não usar desse modo, não é bem visto profissionalmente a não ser que irá trabalhar com grandes valores.
+		int x = 1____2____3;
+		int y = 1_2_3;
+		System.out.printf("São iguais: %b\n", (x == y));
+	}
+	
+}
---
+++ Estudo_Java/src/com/valdomiro/curso/salvandoelendoobjetos/DeserializandoObjeto.java
@@ -0,0 +1,28 @@
+package com.valdomiro.curso.salvandoelendoobjetos;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
+import com.valdomiro.curso.salvandoelendoobjetos.model.Pessoa;
+
+
+public class DeserializandoObjeto {
+
+	public static void main(String[] args) {
+		try (ObjectInput in = new ObjectInputStream(new FileInputStream("joao.obj"))) {
+			Pessoa p = (Pessoa) in.readObject();
+			System.out.println("Nome: " + p.getNome());
+			System.out.println("Idade: " + p.getIdade());
+			System.out.println("Profissão: " + p.getProfissao());
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.err.println("Erro convertendo para a classe Pessoa");
+			e.printStackTrace();
+		}
+	}
+	
+}

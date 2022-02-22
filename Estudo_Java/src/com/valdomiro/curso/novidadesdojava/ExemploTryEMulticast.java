b67888e8c6d11a01df2e37b24e8de62b937eaa24
Valdomiro Medeiros Costa <valdomiromc@yahoo.com.br>
2022-02-22 06:23:06 -0300

    Chat
    Criando um simples chat

---
+++ Estudo_Java/src/com/valdomiro/curso/novidadesdojava/ExemploTryEMulticast.java
@@ -0,0 +1,24 @@
+package com.valdomiro.curso.novidadesdojava;
+
+import java.util.Scanner;
+
+public class ExemploTryEMulticast {
+	
+	public static void main(String[] args) {
+		try (Scanner entrada = new Scanner(System.in)) {
+		
+			System.out.print("Informe seu nome: ");
+			String nome = entrada.nextLine();
+			
+			System.out.print("Informe sua idade: ");
+			String idade = entrada.nextLine();
+			
+			try {
+				SalvarAluno.salvar(nome, idade);
+			} catch (NegocioException | NumberFormatException e) {
+				System.err.println("Impossível completar operação. Motivo: " + e.getMessage());
+			} 
+		} 
+	}
+
+}
